714e029aecaed200b24426f2ae5bea24e0e665b8
lennartdeckert2301 <lennart.deckert@web.de>
2025-12-01 09:30:32 +0100

    Test Links einbauen

--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+756080bea873265d72732ce687ede2ccc873c07c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a61df01bde34d71bf81f9b713e655dd16516952")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
