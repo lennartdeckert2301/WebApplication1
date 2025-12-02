d7772758c30f70296989f8d1e7877903e61c52e4
lennartdeckert2301 <lennart.deckert@web.de>
2025-12-02 12:46:49 +0100

    Quiz und Erkärungen eingefügt

--- obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
+++ obj/Debug/net10.0/WebApplication1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d4973f55887cae94915623cdf3b7aac8da87925")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8683b634741f19d5caa1735c6809ae031864cbc2")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
